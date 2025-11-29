0cbecb2ff5f9cdadba2d5e588d87e07fcddbd1cc
cr7jrsuii <195530488+cr7jrsuii@users.noreply.github.com>
2025-11-29 21:05:41 +0100

    Update classicfreecam.cpp

--- classicfreecam.cpp
+++ classicfreecam.cpp
@@ -1,3 +1,7 @@
+// Made by Meatboxing(Meatboxer) https://www.youtube.com/@meatboxing
+// License: Attribution-NonCommercial 4.0 International (CC BY-NC 4.0) https://creativecommons.org/licenses/by-nc/4.0/
+// Subscrube
+
 #include <windows.h>
 #include <math.h>
 #include <Xinput.h>
